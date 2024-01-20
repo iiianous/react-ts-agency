b9274febbd55eac4ddea5ff4fe07527a18562096
Ian Wilmae P. Mostar <ianmostar@gmail.com>
2024-01-20 18:07:21 +0800

    feat: change section background colors

--- src/pages/FrontPage.tsx
+++ src/pages/FrontPage.tsx
@@ -2,7 +2,7 @@ function FrontPage() {
   return (
     <>
       <div className="h-full w-full mx-auto p-2 m-0">
-        <section className="bg-gray-800">
+        <section className="bg-gray-600">
           <header className="flex justify-center items-center text-black p-3 text-white">
             <div className="flex-1 font-bold tracking-wide">AGENCY</div>
             <nav className="flex-1">
@@ -30,7 +30,7 @@ function FrontPage() {
               </ul>
             </nav>
             <div className="flex-1">
-              <button className="border-2 border-white px-7 py-2 bg-transparent font-bold">
+              <button className="border-2 border-white px-7 py-2 bg-transparent font-bold hidden lg:block">
                 Contact Us
               </button>
             </div>
@@ -67,12 +67,16 @@ function FrontPage() {
           </div>
           <div className="flex items-center justify-center">
             <div className="w-5/6">
-              <img src="https://fakeimg.pl/1400x720" alt="" />
+              <img
+                className="inline-block"
+                src="https://fakeimg.pl/1400x720"
+                alt=""
+              />
             </div>
           </div>
         </section>
 
-        <section className="bg-gray-800 py-24">
+        <section className="bg-gray-600 py-24">
           <div className="lg:w-5/6 lg:mx-auto flex justify-center items-start flex-col lg:flex-row">
             <div className="lg:w-1/2 flex-items p-10 text-left">
               <h2 className="text-4xl mb-10">What services do we offer?</h2>
@@ -121,14 +125,14 @@ function FrontPage() {
           </div>
         </section>
 
-        <section className="my-10 py-24">
+        <section className="bg-gray-700 py-24">
           <div className="pb-12">
             <p className="mb-10">Testimonials</p>
             <h3 className="text-3xl">Our Client Reviews</h3>
           </div>
           <div className="lg:w-5/6 mx-auto flex flex-wrap">
             <div className="lg:w-1/3 md:w-1/3 mb-6 items-center justify-center">
-              <div className="p-8 text-center mx-2 bg-gray-700">
+              <div className="p-8 text-center mx-2 bg-gray-600">
                 <img
                   className="rounded-full text-center inline-block mb-4"
                   src="https://fakeimg.pl/40x40"
@@ -136,14 +140,14 @@ function FrontPage() {
                 />
                 <p className="font-bold">Mr. Mostar</p>
                 <p className="text-sm mb-6">Retail Company</p>
-                <p className="text-gray-600">
+                <p className="text-gray-400">
                   "Than you for realizing the idea and highly recommended it for
                   those of you who want to create a system."
                 </p>
               </div>
             </div>
             <div className="lg:w-1/3 md:w-1/3 mb-6 items-center justify-center">
-              <div className="p-8 text-center mx-2 bg-gray-700">
+              <div className="p-8 text-center mx-2 bg-gray-600">
                 <img
                   className="rounded-full text-center inline-block mb-4"
                   src="https://fakeimg.pl/40x40"
@@ -151,14 +155,14 @@ function FrontPage() {
                 />
                 <p className="font-bold">Mr. Mostar</p>
                 <p className="text-sm mb-6">Retail Company</p>
-                <p className="text-gray-600">
+                <p className="text-gray-400">
                   "Than you for realizing the idea and highly recommended it for
                   those of you who want to create a system."
                 </p>
               </div>
             </div>
             <div className="lg:w-1/3 md:w-1/3 mb-6 items-center justify-center">
-              <div className="p-8 text-center mx-2 bg-gray-700">
+              <div className="p-8 text-center mx-2 bg-gray-600">
                 <img
                   className="rounded-full text-center inline-block mb-4"
                   src="https://fakeimg.pl/40x40"
@@ -175,7 +179,7 @@ function FrontPage() {
           </div>
         </section>
 
-        <section className="flex flex-col items-center py-48 bg-gray-700">
+        <section className="bg-gray-600 flex flex-col items-center py-48">
           <div className="flex-item">
             <h3 className="text-4xl mb-7">
               Subscribe to get information, latest news and other interesting
@@ -190,13 +194,13 @@ function FrontPage() {
                 placeholder="Your Email"
               />
             </div>
-            <div className="flex-item">
+            <div className="flex-item lg:pl-6">
               <button className="p-3 px-5">Subscribe</button>
             </div>
           </div>
         </section>
 
-        <footer className="flex justify-center bg-gray-700 flex-col lg:flex-row text-center lg:text-left">
+        <footer className="bg-gray-700 flex justify-center flex-col lg:flex-row text-center lg:text-left">
           <div className="flex-item p-4 px-6">Agency</div>
           <div className="flex flex-item items-center justify-center">
             <ul className="flex m-0 p-0 text-center">
