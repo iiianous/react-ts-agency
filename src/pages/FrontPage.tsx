9dd38408f4637b2044bef5232220c6ad34700d9f
Ian Wilmae P. Mostar <ianmostar@gmail.com>
2024-01-20 13:34:55 +0800

    feat: implement responsive section wrappers

--- src/pages/FrontPage.tsx
+++ src/pages/FrontPage.tsx
@@ -2,8 +2,8 @@ function FrontPage() {
   return (
     <>
       <div className="h-full w-5/6 mx-auto p-2 m-0">
-        <section className="bg-gray-800">
-          <header className="flex justify-center items-center text-black p-3">
+        <section className="bg-gray-800 h-4/5">
+          <header className="flex justify-center items-center text-black p-3 text-white">
             <div className="flex-1">AGENCY</div>
             <div className="flex-1">Links</div>
             <div className="flex-1">CTA</div>
@@ -13,7 +13,7 @@ function FrontPage() {
             className="py-24 justify-center items-center"
           >
             <div className="text-center">
-              <h1 className="text-6xl mb-10">
+              <h1 className="text-8xl mb-10">
                 Make your <br />
                 Platform Perfect
               </h1>
@@ -27,7 +27,7 @@ function FrontPage() {
         </section>
 
         <section className="bg-gray-700">
-          <div className="flex justify-center items-center">
+          <div className="flex justify-center items-center flex-col lg:flex-row">
             <div className="flex-items p-10">
               <h2 className="text-4xl">Why should choose us?</h2>
             </div>
@@ -44,8 +44,8 @@ function FrontPage() {
         </section>
 
         <section className="bg-gray-800">
-          <div className="flex justify-center items-center">
-            <div className="w-1/2 flex-items p-10 text-left">
+          <div className="flex justify-center items-center flex-col lg:flex-row">
+            <div className="lg:w-1/2 flex-items p-10 text-left">
               <h2 className="text-4xl mb-10">What services do we offer?</h2>
               <p className="mb-10">
                 These are some of the services that you will get if you
@@ -55,7 +55,7 @@ function FrontPage() {
                 Sell More
               </a>
             </div>
-            <div className="w-1/2 flex flex-items p-10 flex-wrap">
+            <div className="lg:w-1/2 flex flex-items p-10 flex-wrap">
               <div className="text-left w-1/2 mb-10">
                 <img className="mb-5" src="https://fakeimg.pl/50x50" alt="" />
                 <h3 className="mb-4">Content Strategry</h3>
@@ -98,7 +98,7 @@ function FrontPage() {
             <h3 className="text-3xl">Our Client Reviews</h3>
           </div>
           <div className="flex flex-wrap">
-            <div className="w-1/3 items-center justify-center">
+            <div className="lg:w-1/3 mb-6 items-center justify-center">
               <div className="p-8 text-center mx-2 bg-gray-700">
                 <img
                   className="rounded-full text-center inline-block mb-4"
@@ -113,7 +113,7 @@ function FrontPage() {
                 </p>
               </div>
             </div>
-            <div className="w-1/3 items-center justify-center">
+            <div className="lg:w-1/3 mb-6 items-center justify-center">
               <div className="p-8 text-center mx-2 bg-gray-700">
                 <img
                   className="rounded-full text-center inline-block mb-4"
@@ -128,7 +128,7 @@ function FrontPage() {
                 </p>
               </div>
             </div>
-            <div className="w-1/3 items-center justify-center">
+            <div className="lg:w-1/3 mb-6 items-center justify-center">
               <div className="p-8 text-center mx-2 bg-gray-700">
                 <img
                   className="rounded-full text-center inline-block mb-4"
@@ -153,7 +153,7 @@ function FrontPage() {
               offers about the agency
             </h3>
           </div>
-          <div className="flex flex-item">
+          <div className="flex flex-item flex-col lg:flex-row">
             <div className="flex-item">
               <input
                 type="text"
@@ -167,8 +167,8 @@ function FrontPage() {
           </div>
         </section>
 
-        <footer className="flex justify-center bg-gray-700">
-          <div className=" p-4 px-6">Agency</div>
+        <footer className="flex justify-center bg-gray-700 flex-col lg:flex-row text-center lg:text-left">
+          <div className="flex-item p-4 px-6">Agency</div>
           <div className="flex flex-item items-center justify-center">
             <ul className="flex m-0 p-0 text-center">
               <li className="flex-item">
